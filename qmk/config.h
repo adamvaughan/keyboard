b16403e08f81e63de2fe2ff97f1e000d8e721ff3
Adam Vaughan <adamjvaughan@gmail.com>
2022-10-08 19:06:40 -0600

    Low profile alice-like.

--- qmk/config.h
+++ qmk/config.h
@@ -7,29 +7,20 @@
 #define DEVICE_VER 0x0001
 
 // the number of rows in your keyboard's matrix
-#define MATRIX_ROWS 10
+#define MATRIX_ROWS 5
 
 // the number of columns in your keyboard's matrix
-#define MATRIX_COLS 6
+#define MATRIX_COLS 12
 
 // pins of the rows, from top to bottom
 #define MATRIX_ROW_PINS \
-    { D4, C6, B3, B2, B6 }
+    { B1, F7, F6, F5, F4 }
 
 // pins of the columns, from left to right
 #define MATRIX_COL_PINS \
-    { D7, E6, B4, B5, F7, B1 }
+    { D1, D0, D4, C6, D7, E6, B4, B5, B7, D5, C7, F1 }
 
 // COL2ROW or ROW2COL
 // how your matrix is configured
 // COL2ROW means the black mark on your diode is facing to the rows and between the switch and the rows
-#define DIODE_DIRECTION COL2ROW
-
-// master half is defined to be the right half
-#define MASTER_RIGHT
-
-#define USE_SERIAL
-
-// when using serial, define this
-// one of `D0`, `D1`, `D2`, `D3`, `E6`
-#define SOFT_SERIAL_PIN D2
+#define DIODE_DIRECTION ROW2COL
--- qmk/rules.mk
+++ qmk/rules.mk
@@ -17,4 +17,3 @@ BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no          # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 OLED_ENABLE = no
-SPLIT_KEYBOARD = yes
